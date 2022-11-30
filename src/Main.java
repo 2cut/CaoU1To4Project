71b67f9f4a6bf27e4f74fa1e1ce9d5a6b20ca4cb
BT_3W2_25 <jialiangc@Nycstudents.net>
2022-11-30 12:30:10 -0500

    Up to XVI Unit 2 req

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello and welcome!\n");
+        String welcome = "Hello and welcome!\n";
+        System.out.println(welcome);
         Reserve plane = new Reserve();
-        Reserve.genReserve();
+        plane.genReserve();
         boolean inUse = true;
         while (inUse) {
             System.out.println("\nVhich seat would you like?");
@@ -18,5 +19,12 @@ public class Main {
             }
         }
         System.out.println("Your total is: "+Reserve.getTotal());
+        String totalString = (int)Reserve.getTotal()+"";
+        //fix bug discount doesn't work
+        if (totalString.length()>4) {
+            System.out.println("Since your total is over $1000, we will give you an $100 dollar discount.");
+            System.out.println(Reserve.getTotal()-100+" is your new total.");
+        }
+        System.out.print(plane.toString());
     }
 }
--- src/Reserve.java
+++ src/Reserve.java
@@ -1,9 +1,17 @@
 public class Reserve {
     private static double total = 0;
+    private String name;
+
+    public Reserve(String guest) {
+        name = guest;
+    }
     public Reserve() {
     }
     private static int k = 1;
-    public static void genReserve() {
+    public String toString() {
+        return "Thank you and have a nice day!";
+    }
+    public void genReserve() {
         for (int i = 0; i<10; i++) {
             for (int j = 1; j<7; j++) {
                 if (j==4) {
@@ -23,13 +31,13 @@ public class Reserve {
     public static void price(int seatnum) {
         if ((seatnum-1)%6==0 || seatnum%6==0) {
             System.out.println("This is a window seat, it will cost extra.");
-            total+= 300.99;
+            total+= 300.99+((int)(Math.random()*10));
         } else if (seatnum%3==0||(seatnum-1)%3==0){
             System.out.println("This is a asle seat, it will cost less.");
-            total+= 200.99;
+            total+= 200.99+((int)(Math.random()*10));
         } else {
             System.out.println("This is a middle seat, it will be regular price.");
-            total+= 250.99;
+            total+= 250.99+((int)(Math.random()*10));
         }
     }
     public static double getTotal () {
